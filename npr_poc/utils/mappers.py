9884ba82744e527d75f82ad1d1a2cf5a9c9f1863
jacobtoppm <jacobtm@torchbox.com>
2020-04-14 17:05:57 +0100

    Update Wagtail and Wagtail Content Import, and change to using the WCI image mapping functionality more directly

--- npr_poc/utils/mappers.py
+++ npr_poc/utils/mappers.py
@@ -3,11 +3,10 @@ from wagtail_content_import.mappers.streamfield import StreamFieldMapper
 
 
 class ImageBlockConverter(BaseConverter):
-    def __call__(self, element, user, *args, **kwargs):
-        image_url = element['value']
-        image_name, image_content = ImageConverter.fetch_image(image_url)
-        title = element.get('title', '')
-        image = ImageConverter.import_as_image_model(title, image_content, owner=user)
+    image_converter = ImageConverter('')
+
+    def __call__(self, element, user, **kwargs):
+        _, image = self.image_converter(element, user=user, **kwargs)
         return (self.block_name, {'caption': '', 'image': image})
 
 
